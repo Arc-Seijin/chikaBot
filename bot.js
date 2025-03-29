7e1b3fbfb96f83034344df9015ee6721e9cc35bf
Arc-Seijin <dhrv.mukh@gmail.com>
2025-03-29 23:21:23 +0530

    Initial commit

---
+++ bot.js
@@ -0,0 +1,18 @@
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+  host: 'Test-LEaV.aternos.me', // Replace with your Aternos server IP
+  port: 31944,                     // Default Minecraft port
+  username: 'TheKeeper69'             // Change this if needed
+});
+
+bot.on('spawn', () => {
+  console.log('Bot has joined the server!');
+});
+
+bot.on('end', () => {
+  console.log('Bot got disconnected, reconnecting...');
+  setTimeout(() => {
+    bot.connect();
+  }, 5000); // Reconnect after 5 seconds
+});
